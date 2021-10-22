8078f48026625f6fd2178544d90c1d074fa58b76
ramir2020 <75266018+ramir2020@users.noreply.github.com>
2021-10-22 22:52:44 +0500

    лабораторные по Веб-технологиям
    лабораторные по Веб-технологиям

---
+++ calc.js
@@ -0,0 +1,101 @@
+﻿var currentValue = "0"; //текущее значение калькулятора (отображается на дисплее)
+var savedValue = "0"; //промежуточное значение
+var signMinus = false; //true - число отрицательное
+var currentOperation = null; //символ текущей операции
+var resetOnNumberPressed = false; //если после нажатия кнопки "=" пользователь нажимает цифру, дисплей сбрасывается до 0
+ 
+var highlightId = null; //id подсвеченного элемента
+ 
+function display() { //вспомагательная функция, обновляет дисплей
+    document.getElementById("display").innerHTML = signMinus ? "-" + currentValue : currentValue;
+}
+ 
+function numPressed(num) { //при нажатии на цифровую кнопку
+    if (resetOnNumberPressed) { //если после нажатия кнопки "=" пользователь нажимает цифру, дисплей сбрасывается до 0
+        reset();
+        resetOnNumberPressed = false;
+    }
+ 
+    //дописываем к текущему значению нажатую цифру
+    if (currentValue == "0")
+        currentValue = String(num);
+    else
+        currentValue += num;
+    display();
+}
+ 
+function reset() { //сброс
+    currentValue = "0";
+    savedValue = "0";
+    signMinus = false;
+    resetHighlight();
+    display();
+}
+ 
+function signChange() { //смена знака
+    if (currentValue == "0") //для 0 знак не меняется
+        return;
+ 
+    signMinus = !signMinus; //смена знака
+    display();
+}
+ 
+function operation(op) { //операция + - / *
+    if (currentOperation != null) //если в памяти есть какая-то незавершенная операция
+        calculate(); //нужно ее выполнить и обновить значение
+    resetOnNumberPressed = false;
+ 
+    //запоминает текущее значение в промежуточный результат
+    savedValue = signMinus ? "-" + currentValue : currentValue;
+    currentValue = 0;
+    signMinus = false;
+    currentOperation = op;
+    display();
+}
+ 
+function calculate() { //выполнить подсчет операции, вспомогательная функция
+    if (currentOperation == null) //операции нет
+        return;
+ 
+    currentValue = signMinus ? "-" + currentValue : currentValue;
+    currentValue = String(eval(savedValue + currentOperation + currentValue)); //вычисляет значение
+ 
+    currentOperation = null;
+    savedValue = 0;
+    signMinus = false;
+    resetOnNumberPressed = true;
+ 
+    if (currentValue[0] == '-') { //если число отрицательное
+        currentValue = currentValue.substring(1);
+        signMinus = true; //ХРАНИМ ЗНАК ТОЛЬКО В ЭТОЙ ПЕРЕМЕННОЙ (чтобы избежать множества глупых ошибок)
+    }
+ 
+    resetHighlight(); //операция выполнена, сбрасываем подсветку операций
+    display();
+}
+ 
+function backspace() { //удалить символ
+    if (resetOnNumberPressed) { //если после нажатия кнопки "=" пользователь нажимает "стереть", дисплей сбрасывается до 0
+        reset();
+        return;
+    }
+ 
+    if (currentValue.length === 1) { //если введен только один символ
+        currentValue = "0"; //обнуляем
+        signMinus = false;
+    } else
+        currentValue = currentValue.substring(0, currentValue.length - 1); //иначе стираем последний
+    display();
+}
+ 
+function highlight(id) { //выделить кнопку с этим id в голубой цвет
+    resetHighlight(); //сначала убираем подсветку с другой кнопки
+ 
+    document.getElementById(id).style.backgroundColor = "lightblue";
+    highlightId = id; //запоминает id подсвеченной кнопки
+}
+ 
+function resetHighlight() { //очистка подсветки, вспомогательная функция
+    if (highlightId != null)
+        document.getElementById(highlightId).style.backgroundColor = "";
+}
---
+++ jquery.myPlugin.js
@@ -0,0 +1,89 @@
+﻿(function( $ ) {
+    var methods = {
+      start : function () {
+        var userChoice = null;
+        var computerChoice = null;
+        $(document).ready(
+          function ()
+          {
+               $('#rock').click(function(){
+                  userChoice = "rock";
+                  newComputerChoice();
+                  computResult();
+              });
+              $('#paper').click(function(){
+                  userChoice = "paper";
+                  newComputerChoice ();
+                  computResult();
+              });
+              $('#scissors').click(function(){
+                  userChoice = "scissors";
+                  newComputerChoice ();
+                  computResult();
+              });
+          }
+      );
+
+      function newComputerChoice ()
+      {
+          computerChoice = Math.floor(Math.random() * 3) + 1;
+          
+          if (computerChoice == 1)
+              {
+                  computerChoice = "rock";
+              }
+          else if (computerChoice == 2)
+              {
+                  computerChoice = "paper";
+              }
+          else 
+              {
+                  computerChoice = "scissors";
+              }
+
+      }
+      function computResult()
+      {
+          var result; 
+          if (userChoice == computerChoice){
+              result="Ничья!";
+          } else if (userChoice == "rock" && computerChoice == "scissors"){
+              result="Вы проиграли :(";
+          } else if (userChoice == "paper" && computerChoice == "rock"){
+              result="Вы проиграли :(";
+          } else if (userChoice == "scissors" && computerChoice == "paper"){
+              result="Вы проиграли :(";
+          } else if (computerChoice == "rock" && userChoice == "scissors"){
+              result="Вы выиграли!";
+          } else if (computerChoice == "paper" && userChoice == "rock"){
+              result="Вы выиграли!";
+          } else if (computerChoice == "scissors" && userChoice == "paper"){
+              result="Вы выиграли!";
+          }
+          var table=document.getElementById("result");
+          var row=table.insertRow(table.rows.length);
+          var cell=row.insertCell(row.cells.length);
+          var img=document.createElement("img");
+          img.src=document.getElementById(computerChoice).src;
+          $(cell).append(img);
+          cell=row.insertCell(row.cells.length);
+          img=document.createElement("img");
+          img.src=document.getElementById(userChoice).src;
+          $(cell).append(img);
+          cell=row.insertCell(row.cells.length);
+          cell.innerHTML=result;
+      }
+        
+}}
+     $.fn.myPlugin = function(method) { 
+        if ( methods[method] ) {
+            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+          } else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+          } else {
+            $.error( 'Метод с именем ' +  method + ' не существует для jQuery.tooltip' );
+          } 
+          return this.each(methods[start]);
+          
+        };
+      })( jQuery );
